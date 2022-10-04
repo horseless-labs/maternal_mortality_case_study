95e7492580390b448e3a04cf0bbebb4ebafe7d47
mireles <mireles@horseless.io>
2022-10-04 12:02:17 -0400

    Add wgets for data since 2000

--- mort_process.sh
+++ mort_process.sh
@@ -15,24 +15,40 @@
 
 #wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2018us.zip
 #7z e mort2018us.zip
-split -l 250000 Mort2018US.PubUse.txt
-python3 mortality_row_parser.py -o "maternal_mortality_2018.csv"
-rm x*
+#split -l 250000 Mort2018US.PubUse.txt
+#python3 mortality_row_parser.py -o "maternal_mortality_2018.csv"
+#rm x*
 
 #wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2017us.zip
 #7z e mort2017us.zip
-split -l 250000 VS17MORT.DUSMCPUB
-python3 mortality_row_parser.py -o "maternal_mortality_2017.csv"
-rm x*
+#split -l 250000 VS17MORT.DUSMCPUB
+#python3 mortality_row_parser.py -o "maternal_mortality_2017.csv"
+#rm x*
 
 #wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2016us.zip
 #7z e mort2016us.zip
-split -l 250000 VS16MORT.DUSMCPUB
-python3 mortality_row_parser.py -o "maternal_mortality_2016.csv"
-rm x*
+#split -l 250000 VS16MORT.DUSMCPUB
+#python3 mortality_row_parser.py -o "maternal_mortality_2016.csv"
+#rm x*
 
 #wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2015us.zip
 #7z e mort2015us.zip
-split -l 250000 VS15MORT.DUSMCPUB
-python3 mortality_row_parser.py -o "maternal_mortality_2015.csv"
-rm x*
+#split -l 250000 VS15MORT.DUSMCPUB
+#python3 mortality_row_parser.py -o "maternal_mortality_2015.csv"
+#rm x*
+
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2014us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2013us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2012us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2011us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2010us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2009us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2008us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2007us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2006us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2005us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2004us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2003us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2002us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2001us.zip
+wget https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/mortality/mort2000us.zip
